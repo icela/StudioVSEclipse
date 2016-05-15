1f399335f29b83359eed7cae044e39e4fa9bcde5
千里冰封 <zhangyinsen@126.com>
2016-05-15 15:40:18 +0800

    using java8

--- app/src/main/java/com/ice1000/asgame/MainActivity.java
+++ app/src/main/java/com/ice1000/asgame/MainActivity.java
@@ -1,13 +1,19 @@
 package com.ice1000.asgame;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+        startActivity(new Intent(this, Game.class));
     }
 }
